9efbc477f9dc649595627298aecb78696d34af17
Emmanuel Opoku-Asante <emmanuelasante95@gmail.com>
2022-07-05 19:11:14 -0700

    Print Alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - print an alphabet in lower case
- * Return: Always 0
+ * main - check the code
+ * Return: Always 0.
 */
 
-void print_alphabet(void)
-{
-	int j, n = 122;
+int main(void)
 
-	for (j = 97;  j <= n; j++)
-	{
-		_putchar(j);
-	}
-	_putchar(10);
+{
+
+	print_alphabet();
+
 	return (0);
+
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_alphabet_x10 - prints 10x the alphabets, in lowercase
- * Return: Always 0
+ * main - check the code.
+ * Return: Always 0.
 */
 
-void print_alphabet_x10(void)
-{
-	int i, j = 0, n = 122;
+int main(void)
 
-	while (j < 10)
-	{
-		for (i = 97; i <= n; i++)
-		{
-			_putchar(i);
-		}
-		_putchar(10);
-		j++;
-	}
+{
+
+	print_alphabet_x10();
+
 	return (0);
+
 }
