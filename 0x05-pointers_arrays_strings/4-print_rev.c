871b44b237f9175f9ecda3b557f16b42f553729d
Emmanuel Opoku-Asante <emmanuelasante95@gmail.com>
2022-07-11 22:34:49 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints in reverse
+ * @s: The input string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+	for (length = length - 1; length >= 0; length--)
+	{
+		_putchar(s[length]);
+	}
+	_putchar('\n');
+}
