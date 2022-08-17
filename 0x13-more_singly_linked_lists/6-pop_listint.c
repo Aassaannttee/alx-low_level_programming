d3fc33727b3fdb9593b1b0988562eafe8df604a8
Emmanuel Opoku-Asante <emmanuelasante95@gmail.com>
2022-08-16 22:19:36 -0700

    C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * pop_listint - This function delete a node at
+ * the beginning of linked list
+ * @head: The head pointer
+ *
+ * Return: The haed node's data
+ */
+
+int pop_listint(listint_t **head)
+{
+	int data;
+
+	listint_t *tmp;
+	listint_t *move;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = *head;
+
+	data = tmp->n;
+
+	move = tmp->next;
+
+	free(tmp);
+
+	*head = move;
+
+	return (data);
+}
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * sum_listint - This function returns the sum of all data(n) of linked list
+ * @head: The head node
+ *
+ * Return: the sum of all data(n) of a linkedlist
+ */
+
+int sum_listint(listint_t *head)
+{
+
+	int sum;
+
+	sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+return (sum);
+}
